a16dd11f8f350c0d11d04bfb8de235a1406258aa
AluheGarcia <aluhegarcia07@gmail.com>
2023-09-28 19:41:28 -0300

    subida de archivos en una nueva rama por errores en la anterior

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {LoginComponent} from './login/login.component';
+import { RecuperarContrasenaComponent } from './recuperar-contrasena/recuperar-contrasena.component';
+import { PatientAbmComponent } from './patient-abm/patient-abm.component';
+import { DashboardAdmiComponent } from './dashboard-admi/dashboard-admi.component';
+const routes: Routes = [
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path:"login", component:LoginComponent},
+    {path:"recuperar-contrasena", component: RecuperarContrasenaComponent},
+    {path:"patient-abm", component: PatientAbmComponent},
+    {path:"dashboard-admi", component: DashboardAdmiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RecuperarContrasenaComponent } from './recuperar-contrasena/recuperar-contrasena.component';
+import { PatientAbmComponent } from './patient-abm/patient-abm.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { DashboardAdmiComponent } from './dashboard-admi/dashboard-admi.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecuperarContrasenaComponent,
+    PatientAbmComponent,
+    DashboardAdmiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
